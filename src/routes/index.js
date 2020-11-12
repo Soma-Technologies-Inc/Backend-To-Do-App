84016815049f001a978acbbe39ce71b663780033
Hyacinthe396 <hmutoni520@daviscollege.com>
2020-11-12 15:36:19 +0200

    - adding endpoint to delete all tasks
    code refactoring

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get('/task', taskController.getAllTasks);
 router.delete('/task/:TaskId', taskController.deleteTask);
+router.delete('/task', taskController.deleteAllTasks);
 router.post('/task',taskController.createTask);
 router.get('/task/:TaskId',taskController.findTask);
 router.get('/task/priority/:TaskPriority', taskController.getByPriority);
